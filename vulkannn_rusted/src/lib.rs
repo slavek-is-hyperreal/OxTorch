bcd1f12953860d4c64e071255f07970c2f688538
Sławek Majcher <slavek.majcher@gmail.com>
2026-03-10 16:37:48 +0100

    chore: rename package to vulkannn_rusted_test v3.2.0

--- vulkannn_rusted/src/lib.rs
+++ vulkannn_rusted/src/lib.rs
@@ -10,9 +10,9 @@ fn rust_greeting(name: &str) -> PyResult<String> {
     Ok(format!("Hello from VulkanNN-Rusted, {}! The Iron Age has begun.", name))
 }
 
-/// The main entry point mapped directly to the `vulkannn_rusted` Python module.
+/// The main entry point mapped directly to the `vulkannn_rusted_test` Python module.
 #[pymodule]
-fn vulkannn_rusted(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn vulkannn_rusted_test(m: &Bound<'_, PyModule>) -> PyResult<()> {
     // Initialize the wgpu engine once upon module load
     backend::init_backend();
     // Initialize memory budgets
